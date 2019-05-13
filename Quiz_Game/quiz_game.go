431431455e3603dfadf579f788e1ac1d304b1c0a
sidkumar99 <47260362+sidkumar99@users.noreply.github.com>
2019-05-13 17:23:39 -0500

    Add files via upload

---
+++ Quiz_Game/quiz_game.go
@@ -0,0 +1,71 @@
+package main
+
+import("fmt"
+		"os"
+		"encoding/csv"
+		"io"
+		"log")
+
+
+type Question struct{
+	question string
+	answer string
+}
+
+
+func main(){
+	bootup()
+
+	questions := parse_csv()
+
+	correct := 0
+	total := 0
+
+	for _,q := range questions{
+		
+		fmt.Printf("%s = ", q.question)
+		
+		var attempt string
+		fmt.Scanf("%s", &attempt)
+		
+		if attempt == q.answer{
+			correct++
+		}
+
+		total ++
+	}
+	
+	results(correct, total)
+}
+
+func results(c int, t int){
+	fmt.Printf("You Got %d Out of %d Questions Right! \n",c,t)
+}
+
+func bootup(){
+	fmt.Println("Welcome to the Quiz!")
+
+}
+
+func parse_csv() []Question {
+	csvFile, _ := os.Open("problems.csv")
+	reader := csv.NewReader(csvFile)
+	//Create questions array to hold questions
+	var questions []Question
+
+	for{
+		line, err := reader.Read()
+		if err == io.EOF{
+			break
+		} else if err != nil{
+			log.Fatal(err)
+		}
+
+		questions = append(questions, Question{
+			question: line[0],
+			answer: line[1],
+		})
+	}
+	
+	return questions
+}
